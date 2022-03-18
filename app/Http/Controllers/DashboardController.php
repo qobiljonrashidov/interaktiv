d4dc56bac8d350ee855b75c6584ef05944f69b8c
Администратор <nnqobiljon@gmail.com>
2022-03-18 16:42:52 +0300

    Initial commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Departments;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+
+
+    public function index(){
+        $alldata=Departments::get();
+        return view('dashboard',compact('alldata'));
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,4 @@
+<?php 
+
+echo "Dashboard ".$name;
+?>
